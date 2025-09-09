47127a40cb17a09829c395d3296cd9a9cc3f7d81
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-09-09 15:45:09 +0100

    Added Makefile with detailed beginner-friendly comments for 42/1337 students

---
+++ 01-basics/03-simple-pattern/megaphone.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+#define GREETING   "Hej,"
+#define NAME       "I’m Aboubakr."
+#define PURPOSE    "I wrote this Makefile tutorial to help you build, rebuild, release, and understand Makefiles in detail "
+
+
+int32_t main()
+{
+    std::cout << GREETING << " " << NAME << std::endl;
+    std::cout << PURPOSE << std::endl;
+
+    return (0);
+}

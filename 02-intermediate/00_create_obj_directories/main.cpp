6419f5d9d9ee224634a6b2c166b0455a9eadb3d4
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-09-10 17:27:02 +0100

    Create obj Directory

---
+++ 02-intermediate/00_create_obj_directories/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "utils.h"
+
+int main() 
+{
+    std::cout << greet("Zombie") << std::endl;
+    return 0;
+}
---
+++ 02-intermediate/00_create_obj_directories/shared.cpp
@@ -0,0 +1,6 @@
+#include "utils.h"
+
+std::string greet(const std::string& name) 
+{
+    return "Hello, " + name + "!";
+}
